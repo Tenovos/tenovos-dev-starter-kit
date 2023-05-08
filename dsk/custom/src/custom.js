b92d20851ee4b4e762f4540525624afac63a384d
ian-tenovos <41912013+ian-tenovos@users.noreply.github.com>
2023-05-08 09:53:42 -0400

    chore: safety commit

--- dsk/custom/src/custom.js
+++ dsk/custom/src/custom.js
@@ -1,5 +1,9 @@
 
 exports.someBusinessLogic = async function someBusinessLogic(customerId, assetId) {
-    const customApp = require(`../../custom/src/${customerId}`);
-    customApp.someBusinessLogic(assetId);
+    try {
+        const customApp = require(`../../custom/src/${customerId}`);
+        customApp.someBusinessLogic(assetId);
+    } catch (error) {
+        console.log('default business logic invoked');
+    }
 }
