19108fdcb4f9e6f6e8076a73cd556490be98e2d5
ian-tenovos <41912013+ian-tenovos@users.noreply.github.com>
2023-05-15 10:00:33 -0400

    feat: cloned avenger custom code to pp custom code initially

---
+++ dsk/custom/src/customer/1653371372758/enqueue.js
@@ -0,0 +1,200 @@
+const _ = require('lodash');
+const AWS = require('aws-sdk');
+const Fetch = require('node-fetch');
+const Path = require('path');
+const Utilities = require('../../../../src/tools/utilities');
+const AuthService = require('../../../../src/libs/auth');
+
+async function sendMessage(message) {
+  console.log('Enqueuing event...', message);
+  // async function sendMessage(event) {
+  //   console.log('Enqueuing event...', event);
+
+  // let parsedMessage = null;
+
+  // try {
+  //   if (event.body) {
+  //     const parsedBody = JSON.parse(event.body);
+  //     parsedMessage = JSON.parse(parsedBody.Message);
+  //     console.log('Enqueuing Event Message: ', parsedMessage);
+  //   }
+  // } catch (error) {
+  //   console.log('Could not parse event body', {
+  //     event,
+  //   }, error);
+  // }
+
+  // if (!parsedMessage) {
+  if (!message) {
+    console.log('event does not have a body, not forwarding to queue');
+    return;
+  }
+
+  const params = {
+    DelaySeconds: 0,
+    MessageBody: JSON.stringify(message),
+    QueueUrl: process.env.QUEUE_URL,
+  };
+
+  const sqsClient = new AWS.SQS({
+    region: 'us-east-1',
+  });
+
+  console.log('Sending SQS Message:', params);
+
+  // eslint-disable-next-line no-await-in-loop
+  const result = await sqsClient.sendMessage(params).promise();
+  console.log('Sent Message to Queue:', result);
+}
+const processInitialStage = async (action) => {
+  // Extract Collection ID
+  const collectionId = action.metadataDenormalized.asset_identifier;
+  if (!collectionId) {
+    throw new Error('Missing Collection ID from Action asset_identifier');
+  }
+
+  // Get Collection
+  const collection = await Utilities.getCollection(collectionId, Fetch);
+
+  // Retrieve Assets from Collection
+  const { collectionDocument } = collection;
+
+  // Loop to Send Messages for Assets
+  const chunkSize = 10;
+  const fileIdChunks = _.chunk(collectionDocument, chunkSize);
+  const assets = [];
+  const skipFileIds = [
+    '00000000-0000-0000-0000-000000000000',
+  ];
+
+  for (let i = 0; i < fileIdChunks.length; i += 1) {
+    const fileIdChunk = fileIdChunks[i];
+    const promises = [];
+
+    for (let j = 0; j < fileIdChunk.length; j += 1) {
+      const fileId = fileIdChunk[j];
+      if (!skipFileIds.includes(fileId)) {
+        promises.push(Utilities.getAsset(fileId, Fetch));
+      }
+    }
+
+    // eslint-disable-next-line no-await-in-loop
+    const assetChunk = await Promise.all(promises);
+    assets.push(...assetChunk);
+  }
+
+  const validFileExtensions = [
+    '.indd',
+  ];
+
+  // const assetIds = assets.map((asset) => asset.objectId);
+  // console.log('Asset Object IDs to process', assetIds);
+  const nextStage = 'process-asset';
+  const processedAssetIds = [];
+
+  // Send Notifications for All Assets
+  for (let i = 0; i < assets.length; i += 1) {
+    const asset = assets[i];
+    const { customerId, objectId, filename } = asset;
+
+    const fileExt = Path.extname(filename.toLowerCase());
+
+    // Only send message for Valid File Types
+    if (validFileExtensions.includes(fileExt.toLowerCase())) {
+      const messageBody = {
+        customerId,
+        actionId: action.objectId,
+        collectionId,
+        objectId,
+        filename,
+        stage: nextStage,
+      };
+
+      // eslint-disable-next-line no-await-in-loop
+      await sendMessage(messageBody);
+
+      // Add Asset to Processed Assets List
+      processedAssetIds.push(objectId);
+    } else {
+      console.log(`Skip sending ${nextStage} Notification for Invalid Asset:`, {
+        customerId,
+        objectId,
+        filename,
+      });
+    }
+  }
+
+  const bucket = process.env.CONFIG_BUCKET;
+
+  // Upload Manifest File if any Assets were processed
+  if (processedAssetIds.length) {
+    const actionId = action.objectId;
+    const key = `to-be-processed/${actionId}/${collectionId}/manifest.json`;
+    const body = {
+      actionId,
+      collectionId,
+      objectIds: processedAssetIds,
+    };
+
+    // Upload Manifest File to S3
+    // eslint-disable-next-line no-await-in-loop
+    await Utilities.s3PutObject(bucket, key, body);
+  }
+};
+
+const enqueue = async (event) => {
+  // console.log('enqueue', event);
+
+  try {
+    const body = JSON.parse(event.body);
+
+    if (!body) {
+      throw new Error('Missing Event Body');
+    }
+    const bodyMessage = JSON.parse(body.Message);
+    const stage = Utilities.getStage(bodyMessage);
+    await AuthService.setEnv();
+    await AuthService.tenovosAuth(Fetch);
+    let action = null;
+    if (stage === 'initial') {
+      const actionId = bodyMessage.data.objectId;
+      const maxAttempt = 5;
+      const sleepTime = 30000;
+      let hasMetadata = false;
+      // Loop to Get Action until Action is Indexed with Denormalized Metadata
+      for (let i = 0; i < maxAttempt && !hasMetadata; i += 1) {
+        // eslint-disable-next-line no-await-in-loop
+        action = await Utilities.extractAssetsFromAction(actionId);
+        // Check for Denormalized Metadata, after Action is Indexed
+        if (Object.keys(action.metadataDenormalized).length) {
+          hasMetadata = true;
+        } else {
+          // eslint-disable-next-line no-await-in-loop
+          await Utilities.sleep(sleepTime);
+          console.log('Get Action Retry Attempt:', i + 1);
+        }
+      }
+      if (!hasMetadata) {
+        const message = 'Action is missing Denormalized Metadata';
+        console.error(message, {
+          actionId,
+          maxAttempt,
+          sleepTime,
+        });
+        throw new Error(message);
+      }
+      await processInitialStage(action);
+    } else {
+      const message = JSON.parse(body.Message);
+      await sendMessage(message);
+    }
+  } catch (error) {
+    console.error('Failed to process Action', {
+      event,
+    }, error);
+  }
+};
+
+module.exports = {
+  enqueue,
+};
---
+++ dsk/custom/src/customer/1653371372758/handler.js
@@ -0,0 +1,308 @@
+const _ = require('lodash');
+const AWS = require('aws-sdk');
+const Fetch = require('node-fetch');
+const Path = require('path');
+const Utilities = require('../../../../src/tools/utilities');
+// const AppAuth = require('../../../src/libs/auth');
+
+// const processInitialStage = async (action, stage) => {
+//   // console.log('processInitialStage', asset, stage);
+
+//   try {
+//     // Extract Collection ID
+//     const collectionId = action.metadataDenormalized.asset_identifier;
+//     if (!collectionId) {
+//       throw new Error('Missing Collection ID from Action asset_identifier');
+//     }
+
+//     // Get Collection
+//     const collection = await Utilities.getCollection(collectionId, Fetch);
+
+//     // Retrieve Assets from Collection
+//     const { collectionDocument } = collection;
+
+//     // Loop to Send Messages for Assets
+//     const chunkSize = 10;
+//     const fileIdChunks = _.chunk(collectionDocument, chunkSize);
+//     const assets = [];
+//     const skipFileIds = [
+//       '00000000-0000-0000-0000-000000000000',
+//     ];
+//     const validFileExtensions = [
+//       '.indd',
+//     ];
+
+//     for (let i = 0; i < fileIdChunks.length; i += 1) {
+//       const fileIdChunk = fileIdChunks[i];
+//       const promises = [];
+
+//       for (let j = 0; j < fileIdChunk.length; j += 1) {
+//         const fileId = fileIdChunk[j];
+//         if (!skipFileIds.includes(fileId)) {
+//           promises.push(Utilities.getAsset(fileId, Fetch));
+//         }
+//       }
+
+//       // eslint-disable-next-line no-await-in-loop
+//       const assetChunk = await Promise.all(promises);
+//       assets.push(...assetChunk);
+//     }
+
+//     // const assetIds = assets.map((asset) => asset.objectId);
+//     // console.log('Asset Object IDs to process', assetIds);
+//     const nextStage = 'process-asset';
+
+//     // Send Notifications for All Assets
+//     for (let i = 0; i < assets.length; i += 1) {
+//       const asset = assets[i];
+//       const { customerId, objectId, filename } = asset;
+
+//       const fileExt = Path.extname(filename.toLowerCase());
+
+//       // Only send message for Valid File Types
+//       if (validFileExtensions.includes(fileExt.toLowerCase())) {
+//         const messageBody = {
+//           customerId,
+//           objectId,
+//           filename,
+//           stage: nextStage,
+//         };
+//         const params = {
+//           DelaySeconds: 0,
+//           MessageBody: JSON.stringify(messageBody),
+//           QueueUrl: process.env.QUEUE_URL,
+//         };
+
+//         console.log('Sending SQS Message:', params);
+
+//         // const sqsClient = new AWS.SQS({
+//         //   region: 'us-east-1',
+//         // });
+
+//         // const result = await sqsClient.sendMessage(params).promise();
+//         // console.log('added to queue', result);
+//       } else {
+//         console.log(`Skip sending ${nextStage} Notification for Asset:`, {
+//           customerId,
+//           objectId,
+//           filename,
+//         });
+//       }
+//     }
+//   } catch (error) {
+//     console.error('Failed to process Collection and send Asset Messages', {
+//       asset: action,
+//       stage,
+//     }, error);
+//   }
+// };
+
+const processAsset = async (asset) => {
+  console.log('Processing Asset', asset.objectId);
+
+  const missingLinks = [];
+
+  try {
+    // Extract INDD Table Metadata
+    const inddLinksTableMissing = asset.metadataDenormalized.system_layout_links;
+
+    for (let i = 0; i < inddLinksTableMissing.length; i += 1) {
+      const linkRow = inddLinksTableMissing[i];
+
+      const missingLinkFilename = Path.basename(linkRow.link_file_path);
+      const missingLinkFilesize = Path.basename(linkRow.link_file_size);
+      console.log(`Checking Entry ${missingLinkFilename}`);
+      // if (relsFileNames.includes(missingLinkFilename)) {
+      if (linkRow.link_file_status !== 'MISSING') {
+        console.log(`Link already established ${missingLinkFilename}`);
+      } else {
+        console.log(`Need to find link for ${missingLinkFilename}`);
+        const searchTerm = [];
+
+        const filename = missingLinkFilename.replace('-fpo.png', '');
+        searchTerm.push(`${filename} `);
+        // also do an OR search on ABC.JPG and ABC-fpo.EXT -> filename.replaceLastInstaceOf("-fpo.", "");
+        const searchOptions = {
+          excludes: [
+            'metadataDocument.text_content',
+            'technicalMetadata',
+          ],
+        };
+
+        // Search for Placed Graphic Assets
+        // eslint-disable-next-line no-await-in-loop
+        const searchResults = await Utilities.runKeywordSearch(searchTerm, searchOptions, Fetch);
+        const { result } = searchResults;
+        console.log(`Search returned ${result.length} Assets`);
+
+        // Loop to Extract Missing Links
+        for (let j = 0; j < result.length; j += 1) {
+          const pgAsset = result[j];
+          let skipAsset = false;
+
+          // Skip Linked INDD Files
+          if (pgAsset.filename.toLowerCase().endsWith('.indd')) {
+            skipAsset = true;
+          }
+
+          const inddObjId = asset.objectId;
+          const inddFilename = asset.filename;
+          const inddLinkGroup = _.get(asset, 'technicalMetadata.image_metadata.assetLinksLinkGroup', 'unknown');
+          const inddRecId = _.get(asset, 'technicalMetadata.image_metadata.recordId', 'unknown');
+          let pgObjIds = [
+            pgAsset.objectId,
+          ];
+          const pgFlName = pgAsset.filename;
+          const pgFlSize = _.get(pgAsset, 'technicalMetadata.tenovos_metadata.fileSizeInBytes', 'unknown');
+          const pgUsedInd = _.get(pgAsset, 'technicalMetadata.image_metadata.usedIn', 'unknown');
+          const pgLinkGroup = _.get(pgAsset, 'technicalMetadata.image_metadata.assetLinksLinkGroup', 'unknown');
+
+          try {
+            // Check for Uncomfirmed Object IDs
+            const pgUnconfirmedObjectIds = JSON.parse(
+              _.get(pgAsset, 'metadataDenormalized.system_layout_links.link_unconfirmed_object_ids', '[]'),
+            );
+            // Use Unconfirmed Object IDs instead of Filename if exists
+            if (Array.isArray(pgUnconfirmedObjectIds) && pgUnconfirmedObjectIds.length) {
+              pgObjIds = pgUnconfirmedObjectIds;
+            }
+          } catch (error) {
+            console.warn('Failed to extract Unconfirmed Links from Asset', {
+              objectId: asset.objectId,
+            });
+          }
+
+          // pgCandidate.filename !== 'Augmented Icon_100.ai'
+          // if (!pgAsset || !pgAsset.technicalMetadata || !pgAsset.technicalMetadata.image_metadata
+          //   || !pgAsset.technicalMetadata.image_metadata.fileSize) {
+          //   console.log('Got It');
+          // }
+
+          // Add Missing Link if Not Skipping Asset
+          if (!skipAsset) {
+            // Loop to create Missing Links for Each Link Object ID
+            for (let k = 0; k < pgObjIds.length; k += 1) {
+              const pgObjId = pgObjIds[k];
+
+              const missingLink = `${inddObjId}\t${inddFilename}\t${inddLinkGroup}\t${inddRecId}`
+                + `\t${missingLinkFilename}\t${missingLinkFilesize}\tmissing\t${pgObjId}\t${pgFlName}\t${pgFlSize}`
+                + `\t${pgUsedInd}\t${pgLinkGroup}`;
+
+              // Add Missing Link to List
+              missingLinks.push(missingLink);
+            }
+          }
+        }
+      }
+    }
+  } catch (error) {
+    console.error('Failed to Process Asset and Extract Missing Links', asset, error);
+    throw error;
+  }
+
+  console.log('Missing Links', missingLinks);
+
+  return missingLinks;
+};
+
+const writeObjectToS3 = async (bucket, object) => new Promise((resolve, reject) => {
+  const { actionId, collectionId, objectId } = object;
+
+  console.log(`AWS_PROFILE: ${process.env.AWS_PROFILE}`);
+  const s3 = new AWS.S3({
+    region: 'us-east-1',
+  });
+  const params = {
+    Bucket: bucket,
+    Key: `to-be-processed/${actionId}/${collectionId}/${objectId}.json`,
+    Body: JSON.stringify(object),
+    ContentType: 'application/json',
+  };
+  console.log('Writing to s3', params);
+  s3.putObject(
+    params,
+    (error, data) => {
+      if (error) {
+        const message = 'Failed to Upload Object to S3';
+        console.error(message, {
+          bucket,
+          actionId,
+          collectionId,
+          objectId,
+        }, JSON.stringify(error));
+        reject(new Error(message));
+      } else {
+        console.log('Uploaded Object to S3:', {
+          bucket,
+          actionId,
+          collectionId,
+          objectId,
+        }, JSON.stringify(data));
+        resolve(data);
+      }
+    },
+  );
+});
+
+const uploadMissingLinks = async (request, missingLinks) => {
+  const { actionId, collectionId, objectId } = request;
+  const bucket = process.env.CONFIG_BUCKET;
+
+  const params = {
+    actionId,
+    collectionId,
+    objectId,
+    csv: missingLinks,
+  };
+  await writeObjectToS3(bucket, params);
+  console.log('Completed Upload of Missing Links for Asset', {
+    actionId,
+    collectionId,
+    objectId,
+  });
+};
+
+const someBusinessLogic = async (request, stage) => {
+  console.log('Proces Custom Logic for Asset', {
+    request,
+    stage,
+  });
+  // const accountIdSecrets = JSON.parse(process.env.SECRETS);
+  // await AppAuth.tenovosAuth(Fetch);
+
+  // const response = await Utilities.getAsset(assetId, Fetch);
+  // console.log(`Asset object: \nJSON.stringify(${response})`);
+
+  // Choose Operation Based on Stage
+  // if (stage === 'initial') {
+  //   await processInitialStage(asset);
+  // } else if (stage === 'process-asset') {
+  // await processAsset(request);
+  // }
+
+  try {
+    const { objectId } = request;
+
+    // Get Asset by Object ID
+    const asset = await Utilities.getAsset(objectId, Fetch);
+    // console.log('INDD Asset:', asset);
+
+    // Process Asset to Generate Missing Links JSON
+    const missingLinks = await processAsset(asset);
+
+    // Upload Missing Links to S3
+    await uploadMissingLinks(request, missingLinks);
+
+    // Write Asset Output to S3
+  } catch (error) {
+    console.error(
+      'Failed to process Asset',
+      request,
+      error,
+    );
+  }
+};
+
+module.exports = {
+  someBusinessLogic,
+};
