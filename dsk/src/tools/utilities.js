f0dbd3ee9033fecf064f89ddc8bb2461636fa829
ian-tenovos <41912013+ian-tenovos@users.noreply.github.com>
2023-05-16 11:52:55 -0400

    chore: reintroduce missing 404 check

--- dsk/src/tools/utilities.js
+++ dsk/src/tools/utilities.js
@@ -118,7 +118,7 @@ const getAsset = async (assetId, nodeFetch) => {
     getAssetOptions.url,
     getAssetOptions,
   );
-  if (!response.ok) {
+  if (!response.ok && response.status !== 404) {
     throw new Error(
       `HTTP error! status: ${response.status} statusText: ${response.statusText} `,
     );
